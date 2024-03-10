3cbc57a09e0450962c058aa89f38030a8b77ebda
Esteban Z <zam22119@uvg.edu.gt>
2024-03-10 13:18:22 -0600

    refactor: updated version of logs

--- src/logs.js
+++ src/logs.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Definiendo __dirname en ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -14,7 +13,6 @@ const logRequest = (req, res, next) => {
     const duration = new Date() - start;
     const logMessage = `${start.toISOString()}, ${method} ${url}, Payload: ${JSON.stringify(body)}, Status: ${res.statusCode}, Duration: ${duration}ms\n`;
 
-    // Usar __dirname definido previamente para especificar la ruta al archivo de log
     fs.appendFile(path.join(__dirname, 'log.txt'), logMessage, (err) => {
       if (err) {
         console.error('Error logging request:', err);
